f0003404db9e9203854ea7473ab41194196ebec8
rammah karpous <rammahkarpous@outlook.com>
2024-06-03 18:04:10 +0200

    fix: addUserToDatabase function fails because user already exists

--- actions/checkIfUserIdExistsInDb.ts
+++ actions/checkIfUserIdExistsInDb.ts
@@ -5,28 +5,29 @@ import prisma from "@/lib/db";
 import { addUserToDatabase } from "./addUserToDatabase";
 
 export const checkIfUserIdExistsInDb = async () => {
-    const user = await currentUser();
-    const users = await prisma.user.findMany();
+    try {
+        const user = await currentUser();
+        const users = await prisma.user.findMany();
 
-    const createUser = {
-        user_id: user?.id!,
-    }
+        const createUser = {
+            user_id: user?.id!,
+        };
 
+        if (users.length >= 1) {
+            const userId = users.find(userInPrisma => user?.id === userInPrisma?.user_id);
 
-    if ( users.length >= 1 ) {
-        users.map(userInPrisma => {
-            if ( user?.id === userInPrisma?.user_id) {
-                return false 
+            if (!userId) {
+                console.log("User does not exists");
+                addUserToDatabase(createUser);
             }
-            else { 
-                addUserToDatabase(createUser) 
-            }
-        })
-    } else {
-        addUserToDatabase(createUser)
-    }
+        } else {
+            addUserToDatabase(createUser);
+        }
 
-    return users;
+        return users;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 // griftdijk12
